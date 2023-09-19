716ad6b8ff502fc947b08892d5eb582938b4b82c
carly509 <tesnorcarly@gmail.com>
2023-09-19 11:25:04 -0400

    score can be updtated

--- game.rb
+++ game.rb
@@ -4,7 +4,7 @@ SNAKE_SIZE = 10
 GRID_WIDTH = Window.width / SNAKE_SIZE
 GRID_HEIGHT = Window.height / SNAKE_SIZE
 
-set background: 'random'
+set background: 'black'
 
 class Snake
     attr_writer :direction
@@ -21,13 +21,13 @@ class Snake
         @positions.shift
         case @direction
         when 'down'
-            @positions.push([@positions.last[0], @positions.last[1] + 1])
+            @positions.push(new_coordinates(snake_head[0], snake_head[1] + 1))
         when 'up'
-            @positions.push([@positions.last[0], @positions.last[1] - 1])
+            @positions.push(new_coordinates(snake_head[0], snake_head[1] - 1))
         when 'left'
-            @positions.push([@positions.last[0] - 1, @positions.last[1]])
+            @positions.push(new_coordinates(snake_head[0] - 1, snake_head[1]))
         when 'right'
-            @positions.push([@positions.last[0] + 1, @positions.last[1]])
+            @positions.push(new_coordinates(snake_head[0] + 1, snake_head[1]))
         end
     end
 
@@ -44,6 +44,14 @@ class Snake
         [x % GRID_WIDTH, y % GRID_HEIGHT]
     end
 
+    def x
+        snake_head[0]
+    end
+
+    def y
+        snake_head[1]
+    end
+
     private
     def snake_head
         @positions.last
@@ -53,14 +61,47 @@ class Snake
     end
 end
 
+class Game
+    def initialize
+        @score = 0
+        @level = 1
+        @food_x = rand(GRID_WIDTH)
+        @food_y = rand(GRID_HEIGHT)
+    end
+
+    def draw_food
+        display_score
+        Square.new(x: @food_x * SNAKE_SIZE, y: @food_y * SNAKE_SIZE, size: SNAKE_SIZE, color: 'green')
+    end
+    def display_score
+        Text.new("Score: #{@score}", x: 10, y: 10, color: 'red',size:12)
+        Text.new("Level: #{@level}", x: 10, y: 25, color: 'red',size:12)
+    end
+
+    def food_was_eaten?(x,y)
+        x == @food_x && y == @food_y
+    end
+
+    def record_score
+        @score += 1
+        @food_x = rand(GRID_WIDTH)
+        @food_y = rand(GRID_HEIGHT)
+    end
+end
+
 
 snake = Snake.new
+game = Game.new
 
 #ruby2d built-in event
 update do
     clear
     snake.draw
     snake.move
+    game.draw_food
+    if game.food_was_eaten?(snake.x, snake.y)
+        game.record_score
+    end
 end
 
 on :key_down do |e|
